cb82d6d72e2014aed5109297ae77c5d25c116fe9
Nitesh Raut <rautesh6@gmail.com>
2020-11-06 08:19:17 +0545

    [Added] task creation, edition & deleteion feature in task management

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { on } from 'process';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  value = [];
+  inputValue = '';
+
+  onCreateTask(): void {
+    if (this.inputValue) {
+      this.value.push(this.inputValue);
+      this.inputValue = '';
+    }
+  }
+
+  deleteTask(val: string): void {
+    this.value = this.value.filter((value) => value !== val);
+  }
+
+  updateTask(val: string): void {
+    this.onCreateTask();
+    this.deleteTask(val);
+    this.inputValue = val;
+  }
+}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  styleUrls: ['./servers.component.scss'],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
